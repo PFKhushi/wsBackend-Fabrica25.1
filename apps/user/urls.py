47fb92bc98edddc03c2988aa3ebd2c5216e5c120
Pedro Firmino <mafiazuka@gmail.com>
2025-03-01 20:43:12 -0300

    Registro finalizado, mas novos usuarios nao estao autenticados para user a pagina admin

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('login_user', views.user_login, name='login'),
     path('logout_user', views.user_logout, name='logout'),
+    path('register_user', views.register_user, name='register'),
 ]
--- apps/user/views.py
+++ apps/user/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 def user_login(request):
     
@@ -21,3 +22,18 @@ def user_logout(request):
     logout(request)
     return redirect('index')
     
+    
+def register_user(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid:
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(request, username=username, password=password)
+            login(request, user)
+            messages.success(request, 'Registro salvo')
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'authenticate/register_user.html', {'form': form,})
